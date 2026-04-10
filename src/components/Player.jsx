c2456da575878334aed67cbbce45be18b8aec9e8
Artem Kutsan <artemkutsan@gmail.com>
2026-04-10 10:57:08 +0200

    feat: добавлен редюсер changeVolume и слайдер громкости

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { playPause, setTime } from "../redux/slices/playerSlice";
+import { playPause, setTime, changeVolume } from "../redux/slices/playerSlice";
 
 function Player() {
   const isPlaying = useSelector((state) => state.player.isPlaying);
   const currentTime = useSelector((state) => state.player.currentTime);
   const maxTime = useSelector((state) => state.player.maxTime);
+  const volume = useSelector((state) => state.player.volume);
+  const isMuted = useSelector((state) => state.player.isMuted);
   const dispatch = useDispatch();
 
   return (
@@ -19,6 +21,13 @@ function Player() {
         <input type="range" min={0} max={maxTime} value={currentTime} onChange={(event) => dispatch(setTime(Number(event.target.value)))} />
         <span>
           {currentTime} / {maxTime}
+        </span>
+      </div>
+      <div className="flex items-center gap-4">
+        <label>Громкость:</label>
+        <input type="range" min={0} max={100} value={volume} onChange={(event) => dispatch(changeVolume(Number(event.target.value)))} />
+        <span>
+          {volume}% {isMuted && "(muted)"}
         </span>
       </div>
     </div>
--- src/redux/slices/playerSlice.js
+++ src/redux/slices/playerSlice.js
@@ -23,8 +23,19 @@ const playerSlice = createSlice({
       else if (action.payload > state.maxTime) state.currentTime = state.maxTime;
       else state.currentTime = action.payload;
     },
+    changeVolume: (state, action) => {
+      if (action.payload < 0) state.volume = 0;
+      else if (action.payload > 100) state.volume = 100;
+      else state.volume = action.payload;
+
+      if (state.volume === 0) {
+        state.isMuted = true;
+      } else if (state.isMuted) {
+        state.isMuted = false;
+      }
+    },
   },
 });
 
-export const { playPause, setTime } = playerSlice.actions;
+export const { playPause, setTime, changeVolume } = playerSlice.actions;
 export default playerSlice.reducer;
